1c3ff2f9ef8811f7288171804877929c59c3d71e
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-08-20 22:02:54 +0100

    button customise

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,31 +1,57 @@
+import { Rabbit } from 'lucide-react'
 import React from 'react'
-function Button1(){
+function Button1(props){
+    console.log(props)
   return (
-    <button className="btn btn-primary">button</button>
+    <button className={"btn btn-primary" + props.className}>{props.children}</button>
   )
+}
+
+function ShoeCard(props){
+    return (
+    <div className='bg-neutral text-neutral-content shadow-md card' >
+        <picture>
+        <img className="h-32 w-full object-cover" src={props.image} alt={props.name} />
+      </picture>
+      <div className="card-body">
+        <p className="card-title">
+            {props.name}
+        </p>
+
+      </div>
+
+    </div>
+    
+)
 }
 
 const TestComponent = () => {
 
 
 // return React.createElement(
-//     //type of element
+//     //type of element: string
 //     'div',  
-//     //props
+//     //props : object
 //     { className: 'test-component card bg-neutral' }, 
-//     //children
+//     //children : tableau of children or single child
 //     'Hello, World!',
 //     React.createElement('p', null, 'This is a test component.') 
 // )
 
 
   return (
-    <div className="flex flex-col gap-4 p-4 ">
+    <div className="flex flex-col gap-4 items-start">
       <h1 className="text-2xl font-bold">Hello, World!</h1>
       
-      <Button1 >Click Me</Button1>
-      <Button1 />
-      <Button1 />
+      <Button1 className="btn btn-info btn-lg" text="Je suis super">
+        <Rabbit size={24}/> 
+        Je suis super
+        </Button1>
+
+        <ShoeCard  image="https://picsum.photos/id/237/200/300" name="DOG">
+        
+        </ShoeCard>
+      
     </div>
   )
 }
